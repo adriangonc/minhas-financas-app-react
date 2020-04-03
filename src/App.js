f97d343d8a168bb07d07a1780838b6d501bbc982
Adriano Gonçalves <adriangonc@yahoo.com.br>
2020-04-03 19:09:37 -0300

    Criação do layout da página de cadastro de usuários

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Login from './views/login'
+import CadastroUsuario from './views/cadastroUsuario'
 import './custom.css'
 
 import 'bootswatch/dist/flatly/bootstrap.css'
@@ -9,7 +10,8 @@ class App extends React.Component {
   render(){
     return(
       <div>
-        <Login/>
+        {/* <Login/> */}
+        <CadastroUsuario />
       </div>
     )
   }
