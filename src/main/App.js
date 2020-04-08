9cc1ed63c5240a44959537ab214d0aa0f3992384
Adriano Gonçalves <adriangonc@yahoo.com.br>
2020-04-08 19:12:57 -0300

    Criação de barra de navegação e refatoração de código.

---
+++ src/main/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import '../custom.css'
+import Rotas from './rotas'
+import Navbar from '../components/navbar'
+
+
+import 'bootswatch/dist/flatly/bootstrap.css'
+
+class App extends React.Component {
+
+  render(){
+    return(
+      <>
+      <Navbar></Navbar>
+      <div className="container">
+        <Rotas></Rotas>
+      </div>
+      </>
+    )
+  }
+
+}
+
+
+export default App;
