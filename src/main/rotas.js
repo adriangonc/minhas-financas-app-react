406e920df975ede4e26725a5680b06bb852cf4bf
unknown <adriangonc@yahoo.com.br>
2020-04-14 15:16:09 -0300

    Criação da página home.

--- src/main/rotas.js
+++ src/main/rotas.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Route, Switch, HashRouter } from 'react-router-dom'
 import Login from '../views/login'
+import Home from '../views/home'
 import CadastroUsuario from '../views/cadastroUsuario'
 
 function Rotas(){
     return(
         <HashRouter>
             <Switch>
+                <Route path="/home" component={Home} />
                 <Route path="/login" component={Login} />
                 <Route path="/cadastro-usuarios" component={CadastroUsuario} />
             </Switch>
