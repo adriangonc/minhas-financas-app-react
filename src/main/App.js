1c580a394d37798ded66c9070485e334768b2664
Adriano Gonçalves <adriangonc@yahoo.com.br>
2020-04-27 18:19:34 -0300

    Adição de biblioteca de mensagens toastr e criação de mensagens de erro para login.

--- src/main/App.js
+++ src/main/App.js
@@ -3,8 +3,9 @@ import React from 'react';
 import '../custom.css'
 import Rotas from './rotas'
 import Navbar from '../components/navbar'
+import 'toastr/build/toastr.min.js'
 
-
+import 'toastr/build/toastr.css'
 import 'bootswatch/dist/flatly/bootstrap.css'
 
 class App extends React.Component {
