b19c7e2b3dd80859b64b30cac13fef2f8a0b1fc7
Adriano Gonçalves <adriangonc@yahoo.com.br>
2020-05-12 17:41:25 -0300

    Adição da biblioteca primereact

--- src/main/App.js
+++ src/main/App.js
@@ -8,6 +8,10 @@ import 'toastr/build/toastr.min.js'
 import 'toastr/build/toastr.css'
 import 'bootswatch/dist/flatly/bootstrap.css'
 
+import 'primereact/resources/themes/nova-light/theme.css'
+import 'primereact/resources/primereact.min.css'
+import 'primeicons/primeicons.css'
+
 class App extends React.Component {
 
   render(){
