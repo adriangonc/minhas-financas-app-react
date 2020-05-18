587c879ca17bc8b5c983c37159771730c0add2a0
Adriano Gonçalves <adriangonc@yahoo.com.br>
2020-05-18 18:10:49 -0300

    Criação de hadleChange para tratar eventos dos componestes, encapsulando os eventos.

--- src/views/lancamentos/cadastroLancamentos.js
+++ src/views/lancamentos/cadastroLancamentos.js
@@ -7,9 +7,30 @@ import LancamentoService from '../../app/service/lancamentoService'
 
 class CadastroLancamentos extends React.Component {
 
+    state = {
+        id: null,
+        descricao: '',
+        valor: '',
+        mes: '',
+        ano: '',
+        tipo: '',
+        status: ''
+    }
+
     constructor(){
         super()
         this.service =  new LancamentoService()
+    }
+
+    submit = () => {
+        console.log(this.state)
+    }
+
+    handleChange = (event) => {
+        const value = event.target.value
+        const name = event.target.name
+
+        this.setState({ [name]: value })
     }
 
     render(){
@@ -22,19 +43,34 @@ class CadastroLancamentos extends React.Component {
                 <div>
                     <div className="col-md-12">
                     <FormGroup id="inputDescricao" label="Descrição: *">
-                        <input id="inputDescricao" type="text" className="form-control" />
+                        <input id="inputDescricao" 
+                            type="text" 
+                            className="form-control" 
+                            name="descricao"
+                            value={this.state.descricao}
+                            onChange={this.handleChange}/>
                     </FormGroup>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-md-6">
                         <FormGroup id="iptAno" label="Ano: *"> 
-                            <input id="iptAno" type="number" className="form-control"/>
+                            <input id="iptAno" 
+                                    name="ano"
+                                    type="number" 
+                                    className="form-control"
+                                    value={this.state.ano}
+                                    onChange={this.handleChange}/>
                         </FormGroup>
                     </div>
                     <div className="col-md-6">
                         <FormGroup id="selectMes" label="Mês: *"> 
-                            <SelectMenu id="selectMes" lista={meses} className="form-control"/>
+                            <SelectMenu id="selectMes" 
+                                        value={this.state.mes}
+                                        name="mes"
+                                        onChange={this.handleChange}
+                                        lista={meses} 
+                                        className="form-control"/>
                         </FormGroup>
                     </div>
                 </div>
@@ -42,26 +78,40 @@ class CadastroLancamentos extends React.Component {
                 <div className="row">
                     <div className="col-md-4">
                         <FormGroup id="iptValor" label="Valor: *">
-                            <input id="iptValor" type="number" className="form-control" />
+                            <input id="iptValor" 
+                                    type="number" 
+                                    name="valor"
+                                    value={this.state.valor}
+                                    onChange={this.handleChange}
+                                    className="form-control" />
                         </FormGroup>
                     </div>
 
                     <div className="col-md-4">
                         <FormGroup id="selectTipo" label="Tipo: *">
-                            <SelectMenu id="selectTipos" lista={tipos} className="form-control"/>
+                            <SelectMenu id="selectTipos" 
+                                        name="tipo"
+                                        value={this.state.tipo}
+                                        onChange={this.handleChange}
+                                        lista={tipos} 
+                                        className="form-control"/>
                         </FormGroup>
                     </div>
 
                     <div className="col-md-4">
                         <FormGroup id="selectStatus" label="Status:">
-                            <input  type="text" className="form-control" disabled={true} />
+                            <input  type="text" 
+                                    name="status"
+                                    value={this.state.status}
+                                    className="form-control" 
+                                    disabled={true} />
                         </FormGroup>
                     </div>
                 </div>
             
             <div className="row">
                 <div className="col-md-6">
-                    <button className="btn btn-success">Salvar</button>
+                    <button onClick={this.submit} className="btn btn-success">Salvar</button>
                     <button className="btn btn-danger">Cancelar</button>
                 </div>
             </div>
