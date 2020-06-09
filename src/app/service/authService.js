75d1f6ea053121b5733142c75d99583a143e8a0d
Adriano Gonçalves <adriangonc@yahoo.com.br>
2020-06-09 17:31:41 -0300

    Desenvolvimento do componente pai, provedor de autenticação.

--- src/app/service/authService.js
+++ src/app/service/authService.js
@@ -12,4 +12,12 @@ export default class AuthService {
     static removerUsuarioAutenticado(){
         LocalStorageService.removerItem(USUARIO_LOGADO)
     }
+
+    logar(usuario){
+        LocalStorageService.adicionarItem(USUARIO_LOGADO, usuario)
+    }
+
+    obterUsuarioAutenticado(){
+        return LocalStorageService.obterItem(USUARIO_LOGADO)
+    }
 }
