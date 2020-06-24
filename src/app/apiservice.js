b4246675c8853bde05463904a1359dfe85c321c0
Adriano <adriangonc@hotmail.com>
2020-06-24 17:38:19 -0300

    Adicionando url da api no heroku.

--- src/app/apiservice.js
+++ src/app/apiservice.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const httpClient = axios.create({
-    baseURL: 'http://localhost:8080'
+    //baseURL: 'http://localhost:8080'
+    baseURL: 'https://financas-pessoas-back.herokuapp.com'
 })
 
 class ApiService {
